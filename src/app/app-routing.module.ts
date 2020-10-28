81c248bf1b616e15a77e0ea02135190433d4914c
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-28 17:48:11 +0100

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { PrintComponent } from './core/print/print.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'students', component: PrintComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', pathMatch:'full', redirectTo:'home'},
+  {path: '**', pathMatch:'full', redirectTo:'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './core/home/home.component';
+import { PrintComponent } from './core/print/print.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PrintComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
